ef704225a96f4373ef66de8ac0aee575b70dd9a3
Claudiu HBann <123uidualcnnamreh@gmail.com>
2022-10-08 21:24:33 +0300

    added a logger class which support both files and console

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,12 +1,20 @@
-#include "Core/Test.h"
-
-#include <iostream>
-using namespace std;
+#include "pch.h"
+#include "Utility/Logger.h"
 
 int main() {
-  Test test;
+#ifdef _DEBUG
+	Logger logger;
+#else
+	Logger logger("C:\\Users\\Claudiu HBann\\Desktop\\Logger.log");
+#endif // _DEBUG
 
-  cout << test.Get();
 
-  return 0;
+	logger.Log("Logger", Logger::Type::NONE);
+	logger.Log("Logger", Logger::Type::NONE, Logger::Color::FG_GREEN);
+	logger.Log("Logger", Logger::Type::INFO);
+	logger.Log("Logger", Logger::Type::WARNING);
+	logger.Log("Logger", Logger::Type::ERROR);
+	logger.Log("Logger", Logger::Type::ERROR, Logger::Color::BG_MAGENTA);
+
+	return 0;
 }
