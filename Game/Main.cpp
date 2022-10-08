be2a94fa45dafa6755f6e9911f64d3b3f5c81816
Claudiu HBann <123uidualcnnamreh@gmail.com>
2022-10-08 21:29:16 +0300

    added sdl2 basic, mixer, ttf and image libraries

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -8,13 +8,7 @@ int main() {
 	Logger logger("C:\\Users\\Claudiu HBann\\Desktop\\Logger.log");
 #endif // _DEBUG
 
-
-	logger.Log("Logger", Logger::Type::NONE);
-	logger.Log("Logger", Logger::Type::NONE, Logger::Color::FG_GREEN);
-	logger.Log("Logger", Logger::Type::INFO);
-	logger.Log("Logger", Logger::Type::WARNING);
-	logger.Log("Logger", Logger::Type::ERROR);
-	logger.Log("Logger", Logger::Type::ERROR, Logger::Color::BG_MAGENTA);
+	logger.Log("Start", Logger::Type::INFO);
 
 	return 0;
 }
