4fac7a4719ec6119772924218d803adc5b1ac0d0
Claudiu HBann <123uidualcnnamreh@gmail.com>
2022-10-10 22:30:47 +0300

    added real random numbers method

--- Engine/Utility/Random.h
+++ Engine/Utility/Random.h
@@ -2,14 +2,22 @@
 
 class Random {
 public:
-	Random();
+	inline Random() : mEngine(mGenerator()) {}
 
 	template<typename Integer>
 	inline Integer GetInteger(
 		const Integer min = numeric_limits<Integer>::min(),
 		const Integer max = numeric_limits<Integer>::max()
-	) {
+	) const {
 		return uniform_int_distribution(min, max)(mEngine);
+	}
+
+	template<typename Real>
+	inline Real GetReal(
+		const Real min = numeric_limits<Real>::min(),
+		const Real max = numeric_limits<Real>::max()
+	) const {
+		return uniform_real_distribution(min, max)(mEngine);
 	}
 
 private:
